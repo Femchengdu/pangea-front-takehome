480ee4ee4c01f86c99a8b0e0819a855606d3cdc9
Samuel Ajayi <femiajayi705@gmail.com>
2021-07-24 04:35:04 +0200

    feat: Added product list component

---
+++ src/components/Product/ProductList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ProductList.css";
+
+const ProductList = () => {
+  return (
+    <div class="grid">
+      <div className="item">item1</div>
+      <div className="item">item2</div>
+      <div className="item">item3</div>
+    </div>
+  );
+};
+
+export default ProductList;
