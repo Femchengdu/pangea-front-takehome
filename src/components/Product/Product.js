9f2ac1071ed570529eab9aacef2fc6d7f65380f7
Samuel Ajayi <femiajayi705@gmail.com>
2021-07-28 05:35:00 +0200

    feat: Added show modal button

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./Product.css";
-const Product = ({ imageUrl, price, title }) => {
+
+const Product = ({ imageUrl, price, title, addItemToCart, id, currency }) => {
+  const showModal = (item) => {
+    // Get the modal
+    const modalElement = document.getElementById("myModal");
+    modalElement.style.display = "block";
+    addItemToCart(item);
+  };
+
+  const item = { price, title, id };
   return (
     <div className="product-container">
       <div className="product-details">
@@ -11,12 +20,11 @@ const Product = ({ imageUrl, price, title }) => {
           </div>
         </div>
         <div className="price-container">
-          <p className="product-price">BBD&nbsp;{price}</p>
+          <p className="product-price">
+            {currency}&nbsp;{price}
+          </p>
         </div>
-        <button
-          className="product-button"
-          onClick={() => console.log("Add to cart clicked")}
-        >
+        <button className="product-button" onClick={() => showModal(item)}>
           Add to cart
         </button>
       </div>
