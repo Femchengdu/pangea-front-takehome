b3bb2cf02f5909bee0ffa2d12d3f855561e333ed
Samuel Ajayi <femiajayi705@gmail.com>
2021-07-28 05:29:25 +0200

    refac: Refatored product list to use a hook

--- src/components/Product/ProductList.js
+++ src/components/Product/ProductList.js
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Product from "./Product";
 import "./ProductList.css";
-import { onGetProductsFromPangea } from "../../graphqlHelper/graphqlHelper";
 
-const ProductList = () => {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    const dataCallback = (results) => {
-      if (results.length) {
-        setProducts(results);
-      }
-    };
-    onGetProductsFromPangea(dataCallback);
-  }, []);
+const ProductList = ({ addItemToCart, products, currency }) => {
   const renderProduct = (productData) => {
     const { id, price, image_url: imageUrl, title } = productData;
-    return <Product key={id} price={price} imageUrl={imageUrl} title={title} />;
+    return (
+      <Product
+        key={id}
+        price={price}
+        imageUrl={imageUrl}
+        title={title}
+        id={id}
+        addItemToCart={addItemToCart}
+        currency={currency}
+      />
+    );
   };
 
   return (
