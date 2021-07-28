e83c52409457628c0cbf195c99ead96357e47a9c
Samuel Ajayi <femiajayi705@gmail.com>
2021-07-28 05:40:15 +0200

    feat: Added custom hooks to app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from "react";
 import ProductList from "./components/Product/ProductList";
 import Header from "./components/Layout/Header";
+import NavBar from "./components/Layout/NavBar";
+import Cart from "./components/Cart/Cart";
+import useCartState from "./hooks/useCartState";
+import useCurrencyList from "./hooks/useCurrencyList";
+import useProductsList from "./hooks/useProductsList";
+import "./App.css";
 const App = () => {
+  const { products, setCurrency, currency } = useProductsList();
+  const currencyList = useCurrencyList();
+  const { addItemToCart, cart, incrementCartItem, decrementCartItem } =
+    useCartState(products, currency);
   return (
     <div>
+      <Cart
+        cart={cart}
+        currencyList={currencyList}
+        incrementCartItem={incrementCartItem}
+        decrementCartItem={decrementCartItem}
+        setCurrency={setCurrency}
+      />
+      <NavBar />
       <Header />
 
-      <ProductList />
+      <ProductList
+        addItemToCart={addItemToCart}
+        products={products}
+        currency={currency}
+      />
     </div>
   );
 };
